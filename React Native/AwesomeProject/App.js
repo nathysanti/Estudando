72a4795102314769b052e65b9f9659fd1fc6719a
nathysanti <nathalia.software@gmail.com>
2022-06-20 09:49:44 -0300

    :sparkles: feat: Adicionando a StatusBar

--- React Native/AwesomeProject/App.js
+++ React Native/AwesomeProject/App.js
@@ -1,9 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
+const colorGitHub = '#010409';
+
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
+      <StatusBar backgroundColor = {colorGitHub} barStyle = "light-content" ></StatusBar>
     <View >
       <Text style={styles.text}>Hello World!</Text>
       <StatusBar style="auto" />
@@ -15,10 +18,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#010409',
+    backgroundColor: colorGitHub,
     alignItems: 'center',
     justifyContent: 'center',
-    fontStyle: '#fff',
   },
   text: {
     fontSize: 30,
